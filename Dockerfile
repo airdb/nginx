1255ab69042999cdab4f7baa740bcfee885fd141
xqbumu <xqbumu@163.com>
2022-07-31 19:13:04 +0800

    chore: use openresty with ja3

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,22 @@
 FROM ubuntu:latest
 
-RUN export DEBIAN_FRONTEND=noninteractive && apt-get update && \
-	apt-get install -y git make gcc curl zlib1g-dev libpcre3-dev vim
+RUN export DEBIAN_FRONTEND=noninteractive && apt-get update
+RUN apt-get install -y git make gcc curl wget zlib1g-dev libpcre3-dev vim \
+	libssl-dev perl build-essential wget
 
 WORKDIR /build
 ENV BUILD_DIR /build
+# COPY ./openresty/openresty-1.21.4.1 ${BUILD_DIR}/openresty-1.21.4.1
+# COPY ./repos ${BUILD_DIR}/repos
+COPY build.sh ${BUILD_DIR}
 
-RUN git clone -b OpenSSL_1_1_1-stable https://github.com/openssl/openssl && \
-    git clone -b release-1.23.0 https://github.com/nginx/nginx  && \
-    git clone -b v0.3.0 https://github.com/phuslu/nginx-ssl-fingerprint && \
-    git clone -b v0.10.21  https://github.com/openresty/lua-nginx-module && \
-    git clone -b v0.3.1 https://github.com/vision5/ngx_devel_kit && \
-    git clone -b v2.1 https://github.com/LuaJIT/LuaJIT && \
-    git clone -b v0.07 https://github.com/openresty/lua-upstream-nginx-module && \
-    git clone -b v0.62 https://github.com/openresty/echo-nginx-module
+RUN mv ${BUILD_DIR}/repos/* ${BUILD_DIR}/
+RUN mkdir -p logs
 
+RUN bash build.sh
 
-RUN cd ${BUILD_DIR}/LuaJIT && \
-	make PREFIX=${BUILD_DIR}/LuaJIT && \
-	make install PREFIX=${BUILD_DIR}/ngx_lib
-#RUN make patch && \
-#    make build && \
-#    mkdir logs
+EXPOSE 8443
+EXPOSE 8080
 
-
-EXPOSE 443
-EXPOSE 8444
-
-#CMD ./nginx/objs/nginx -p /build -c nginx-ssl-fingerprint/conf/nginx.conf
-CMD sleep 3600
+# CMD ./nginx/objs/nginx -p /build -c nginx-ssl-fingerprint/conf/nginx.conf
+CMD sleep 7200
--- build.sh
+++ build.sh
@@ -2,42 +2,31 @@
 
 BUILD_DIR=/build
 
-cd ${BUILD_DIR}
+wget https://openresty.org/download/openresty-1.21.4.1.tar.gz
+tar -zxvf openresty-1.21.4.1.tar.gz
 
-function build_luajit() {
-    cd ${BUILD_DIR}/LuaJIT && make clean && rm -rf ${BUILD_DIR}/ngx_lib && mkdir ${BUILD_DIR}/ngx_lib
+wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz
+tar -xvf pcre-8.45.tar.gz
 
-    make PREFIX=${BUILD_DIR}/LuaJIT && make install PREFIX=${BUILD_DIR}/ngx_lib 
-    #&& rm $luajit_lib_path/lib/libluajit-5.1.so*
-    if [[ $? -ne 0 ]];then
-        echo "build juajit fail"
-        exit -1
-    fi
+wget https://www.openssl.org/source/openssl-1.1.1f.tar.gz
+tar -zvxf openssl-1.1.1f.tar.gz
 
-    echo "build juajit successfuly"
-}
+wget https://github.com/phuslu/nginx-ssl-fingerprint/archive/refs/tags/v0.3.0.tar.gz
+tar -zvxf v0.3.0.tar.gz
 
+cd ${BUILD_DIR}/openssl-1.1.1f/
+patch -p1 < ${BUILD_DIR}/openresty-1.21.4.1/patches/openssl-1.1.1f-sess_set_get_cb_yield.patch
 
-export LUAJIT_LIB=${BUILD_DIR}/ngx_lib/lib
-export LUAJIT_INC=${BUILD_DIR}/ngx_lib/include/luajit-2.1
+cd ${BUILD_DIR}/
+patch -f -p1 -d openssl-1.1.1f <nginx-ssl-fingerprint-0.3.0/patches/openssl.1_1_1.patch
+patch -f -p1 -d openresty-1.21.4.1/bundle/nginx-1.21.4 <nginx-ssl-fingerprint-0.3.0/patches/nginx.patch
 
-## Patch
-patch -f -p1 -d openssl < nginx-ssl-fingerprint/patches/openssl.1_1_1.patch
-patch -f -p1 -d nginx < nginx-ssl-fingerprint/patches/nginx.patch
+cd ${BUILD_DIR}/openresty-1.21.4.1/
 
-## Build Nginx
-cd /build/nginx && \
-			ASAN_OPTIONS=symbolize=1 ./auto/configure \
-			--with-http_ssl_module \
-			--with-stream_ssl_module \
-			--with-debug --with-stream \
-			--with-http_v2_module \
-			--with-cc-opt="-fsanitize=address -O -fno-omit-frame-pointer" \
-			--with-ld-opt="-L/usr/local/lib -Wl,-E -lasan" \
-			--with-ld-opt="-L${BUILD_DIR}/ngx_lib" \
-			--with-openssl=${BUILD_DIR}/openssl \
-			--add-module=${BUILD_DIR}/ngx_devel_kit \
-			--add-module=${BUILD_DIR}/nginx-ssl-fingerprint \
-			--add-module=${BUILD_DIR}/lua-nginx-module \
-			--add-module=${BUILD_DIR}/lua-upstream-nginx-module && \
-			make
+## assuming your have 4 spare logical CPU cores
+./configure \
+	--with-debug \
+	--with-openssl=${BUILD_DIR}/openssl-1.1.1f \
+	--with-pcre=${BUILD_DIR}/pcre-8.45 -j2 \
+	--add-module=${BUILD_DIR}/nginx-ssl-fingerprint-0.3.0 && \
+make -j4
